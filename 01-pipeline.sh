e1ccb679ced760a0e29d14fd0c555f25397a2a34
grelade <jacekgrela@gmail.com>
2022-07-12 13:09:22 +0200

    no pipelines

--- 01-pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python scrap_ixs.py --id_min 40 --id_max 41 --overwrite
-python scrap_arts.py --overwrite
-python scrap_users.py --overwrite
--- 02-pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-STARTDATE="2022-07-07"
-ENDDATE=$(date --iso-8601)
-FILE="data/best_neuropa_${STARTDATE}_${ENDDATE}.id"
-
-python scrap_best_link_ids.py --start_date $STARTDATE --tag neuropa --ixs_file $FILE
-python scrap_links_from_link_ids.py --ixs_file $FILE
